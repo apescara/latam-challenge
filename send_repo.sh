1cd898e139c1bfa2fb68c4bcafe78aace466bd67
andrespescara <andrespescara@gmail.com>
2024-10-11 15:49:20 -0300

    send repo to API

---
+++ send_repo.sh
@@ -0,0 +1,4 @@
+curl -d '{ "name": "Andres Pescara", "mail": "andrespescara@gmail.com", "github_url": "https://github.com/apescara/latam-challenge.git"}' -H "Content-Type: application/json" -X POST https://advana-challenge-check-api-cr-k4hdbggvoq-uc.a.run.app/data-engineer
+
+
+# {"status":"OK","detail":"your request was received"}
